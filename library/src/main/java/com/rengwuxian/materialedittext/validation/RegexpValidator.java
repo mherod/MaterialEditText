75a344207fab35fa72690afc8333d7ccf871f9aa
Matthew Herod <matthew.herod@gmail.com>
2017-03-11 12:22:13 +0000

    Code formatting and gradle bump

--- library/src/main/java/com/rengwuxian/materialedittext/validation/RegexpValidator.java
+++ library/src/main/java/com/rengwuxian/materialedittext/validation/RegexpValidator.java
@@ -9,20 +9,20 @@ import java.util.regex.Pattern;
  */
 public class RegexpValidator extends METValidator {
 
-  private Pattern pattern;
+    private Pattern pattern;
 
-  public RegexpValidator(@NonNull String errorMessage, @NonNull String regex) {
-    super(errorMessage);
-    pattern = Pattern.compile(regex);
-  }
+    public RegexpValidator(@NonNull String errorMessage, @NonNull String regex) {
+        super(errorMessage);
+        pattern = Pattern.compile(regex);
+    }
 
-  public RegexpValidator(@NonNull String errorMessage, @NonNull Pattern pattern) {
-    super(errorMessage);
-    this.pattern = pattern;
-  }
+    public RegexpValidator(@NonNull String errorMessage, @NonNull Pattern pattern) {
+        super(errorMessage);
+        this.pattern = pattern;
+    }
 
-  @Override
-  public boolean isValid(@NonNull CharSequence text, boolean isEmpty) {
-    return pattern.matcher(text).matches();
-  }
+    @Override
+    public boolean isValid(@NonNull CharSequence text, boolean isEmpty) {
+        return pattern.matcher(text).matches();
+    }
 }
